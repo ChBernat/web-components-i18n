4ddfcc1a8f8255c1c50c28be15f5f93dea18a73d
Christopher Bernat <bernat.christopher@gmail.com>
2019-03-28 20:06:24 +0100

    got rid of console.log

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const basicConfig = require('./templates/config.json');
 const appRoot = __dirname.split('node_modules')[0] || __dirname;
 const path = require('path');
-console.log(appRoot, 'dsada')
 let config = basicConfig;
 
 try {
